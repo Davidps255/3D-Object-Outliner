fda9d316f42204de21a8862a2cf703dad5e2ad21
Davidps255 <davidps@uci.edu>
2026-06-03 14:51:57 -0700

    Minor optimizations

--- outliner/scripts/camera_linker.gd
+++ outliner/scripts/camera_linker.gd
@@ -2,7 +2,9 @@ extends Camera3D
 
 @export var main_camera: Camera3D
 
+func _ready():
+	fov = main_camera.fov
+
 func _process(_delta):
 	if main_camera:
 		global_transform = main_camera.global_transform
-		fov = main_camera.fov
